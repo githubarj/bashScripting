0a580c7a92353317324f45bbd5fc4053ebd91321
Githuba Jeremy <githuba9520@gmail.com>
2024-02-20 16:36:14 +0300

    login args

--- showuptime.sh
+++ showuptime.sh
@@ -2,8 +2,8 @@
 
 
 showuptime(){
-	up=$(uptime -p | cut -c4-)
-	since=$(uptime -s)
+	local up=$(uptime -p | cut -c4-)
+	local since=$(uptime -s)
 	cat << EOF
 ----------
 This machine has been for ${up}
